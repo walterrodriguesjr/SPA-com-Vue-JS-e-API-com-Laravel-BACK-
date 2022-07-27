f248dbecc1c711336b75bbf3a895233fd04d8b00
walterrodriguesjr <walterrjr.86@gmail.com>
2022-07-27 19:50:27 -0300

    Concluida a parte de login, cadastro de usuario, troca de dados. Projeto pronto para ser usado como template.

--- webservice/routes/api.php
+++ webservice/routes/api.php
@@ -1,9 +1,6 @@
 <?php
 
-use Illuminate\Http\Request;
-use App\User;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Validator;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -15,61 +12,7 @@ use Illuminate\Support\Facades\Validator;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
-/* ROTA DE CADASTRO DE USUÁRIO */
-Route::post('/cadastro', function (Request $request) {
-    $data = $request->all();
-    $validacao = Validator::make($data, [
-        'name' => 'required|string|max:255',
-        'email' => 'required|string|email|max:255|unique:users',
-        'password' => 'required|string|min:6|confirmed',
-    ]);
-
-    /* CONDICIONAL DE VALIDAÇÃO E RETORNO DE ERRO AO USUÁRIO */
-    if($validacao->fails()){
-        return $validacao->errors();
-    }
-
-    /* MÉTODO DE CRIAÇÃO DO USUÁRIO */
-    $user = User::create([
-        'name' => $data['name'],
-        'email' => $data['email'],
-        'password' => bcrypt($data['password']),
-    ]);
-    $user->token = $user->createToken($user->email)->accessToken;
-    return $user;
-});
-
-/* ROTA DE LOGIN */
-Route::post('/login', function (Request $request) {
-    $data = $request->all();
-    $validacao = Validator::make($data, [
-        'email' => 'required|string|email|max:255',
-        'password' => 'required|string',
-    ]);
-
-    /* CONDICIONAL DE VALIDAÇÃO E RETORNO DE ERRO AO USUÁRIO */
-    if($validacao->fails()){
-        return $validacao->errors();
-    }
-
-    /* MÉTODO DE CRIAÇÃO DO USUÁRIO */
-    if(Auth::attempt(['email'=>$data['email'], 'password'=>$data['password']])){
-       $user = auth()->user(); 
-       $user->token = $user->createToken($user->email)->accessToken;
-       return $user;
-    }else{
-        return ['status'=>false];
-    }
-});
-/* rota de autenticação do usuário */
-Route::middleware('auth:api')->get('/usuario', function (Request $request) {
-    return $request->user();
-});
-
-/* rota de atualização de dados do usuário */
-Route::middleware('auth:api')->put('/perfil', function (Request $request) {
-    $user = $request->user();
-    $data = $request->all();
-    return $data;
-});
+Route::post('/cadastro', "UsuarioController@cadastro"); 
+Route::post('/login', "UsuarioController@login"); 
+Route::middleware('auth:api')->get('/usuario', "UsuarioController@usuario"); 
+Route::middleware('auth:api')->put('/perfil', "UsuarioController@perfil");
